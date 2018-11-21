c863efd0686cb25f78239d23285155701e180366
Mattetrix <45236460+Mattetrix@users.noreply.github.com>
2018-11-21 21:08:54 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -3,9 +3,46 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 const prefix - "URSS."
 
+bot.on('ready', function () {
+    console.log("Login success")
+    bot.user.setActivity('Drinking Vodka').catch(console.error)
+});
+
 bot.on('message', msg => {
     if (msg.content.toLowerCase().startsWith(prefix + 'ping' )) {
         msg.channel.send('Pong !');
+    })
+    bot.on('message', message => {
+
+  if (message.content.startsWith('URSS.play')) {
+    // On récupère le premier channel audio du serveur
+    let voiceChannel = message.guild.channels
+      .filter(function (channel) { return channel.type === 'voice' })
+      .first()
+    let args = message.content.split(' ')
+    // On rejoint le channel audio
+    voiceChannel
+      .join()
+      .then(function (connection) {
+        // On démarre un stream à partir de la vidéo youtube
+        let stream = YoutubeStream(args[1])
+        stream.on('error', function () {
+          message.reply("Je n'ai pas réussi à lire cette vidéo :(")
+          connection.disconnect()
+        })
+        
+        connection
+          .playStream(stream)
+          .on('end', function () {
+            connection.disconnect()
+          })
+      })
+  }
+
+})
+  if (msg.content === prefix + "site"){
+        msg.channel.send("https://fr.wikipedia.org/wiki/Union_des_républiques_socialistes_soviétiques")
+       
     }
     if (content.startsWith(prefix + "time")) {
         let daysofweek = ["Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi" ]
