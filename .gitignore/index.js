02a5135ba826635a6f08afe7ade5c3d5fdad133f
Mattetrix <45236460+Mattetrix@users.noreply.github.com>
2018-11-21 15:52:09 +0100

    Add files via upload

---
+++ .gitignore/index.js
@@ -0,0 +1,20 @@
+
+const bot = new Discord.Client();
+const prefix - "URSS."
+bot.login(process.env.TOKEN)
+
+bot.on('message', msg => {
+    if (msg.content.toLowerCase().startsWith(prefix + 'ping' )) {
+        msg.channel.send('Pong !');
+    }
+    if (content.startsWith(prefix + "time")) {
+        let daysofweek = ["Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi" ]
+        msg.channel.send(daysofweek[(new Date ).getDay()])
+    }
+    if (content.startsWith(prefix + "roulette russe")) {
+        msg.channel.send("Et c'est ..." + Math.floor(Math.random() * 6 + 1)
+    if (msg.content == "Et c'est ... 6" )
+    else guildMember.kick
+    guildMember.createDM("https://discord.gg/YYgYZDn")
+  }
+});
